bff311d2ca0218589c1828c3f54c9c96499c6396
yaqingteng <yaqingteng@gmail.com>
2021-12-26 23:02:36 +0800

    update: 完成service的改造和使用

--- protobuf/client.js
+++ protobuf/client.js
@@ -1,39 +1,54 @@
 const net = require('net');
-const {encodeData} = require('./lib')
+const {encodeData, decodeData} = require('./lib')
+const protobuf = require("protobufjs");
 const data = {
     // name: '跑了了可是大姐凡',
     // age: 1,
     // sexEnum: 0
     title: '我是一个文章',
-    view:'123123',
+    view: '123123',
     createdTime: 'wwww',
-    encourage:true,
-    author:{
-        name:'bob'
+    encourage: true,
+    author: {
+        name: 'bob'
     }
 };
+let client = new net.Socket();
 
 const devCloudHost = '9.134.188.69'
 const cvmHost = '101.43.20.68'
 
-let client = new net.Socket();
-client.connect({
-    host: devCloudHost,
-    port: 3344
-});
+async function start() {
+    const root = await protobuf.load(__dirname + '/blog.proto')
+    const BlogMessage = root.lookup('cms.BlogService');
+    const greeter = BlogMessage.create(function (method, requestData, callback) {
+        // perform the request using an HTTP request or a WebSocket for example
+        // decodeData({id: '123'}).then(data => callback(null, data))
+        // and call the callback with the binary response afterwards:
+        // callback(null, responseData);
+        console.log('requestData',requestData)
+        client.write(requestData)
+        client.on('data', data => {
+            // console.log('receive', data.toString());
+            callback(null,data)
+            client.end();
+        });
 
-client.on('connect', async () => {
-    await setMessage(data);
-});
+    }, false, false);
+    client.connect({
+        host: cvmHost,
+        port: 3344
+    });
+    client.on('connect', async () => {
+        greeter.getBlog({id: '9999'}).then(data => {
+            console.log('data', data)
+        })
+        // await setMessage(data);
+    });
 
-client.on('data', data => {
-    console.log('receive', data.toString());
-    client.end();
-});
 
 
-async function setMessage(data) {
-    const buffer = await encodeData(data)
-    // 发送
-    client.write(buffer);
 }
+
+
+start()

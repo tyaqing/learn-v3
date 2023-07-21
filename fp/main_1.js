edc849497737c2aa813ce0205ab74bda3525eba2
yakirteng <yakirteng@tencent.com>
2023-07-21 18:48:35 +0800

    增加fp container

---
+++ fp/main_1.js
@@ -0,0 +1,10 @@
+import _ from 'lodash/fp'
+
+var Container = function(x) {
+    this.__value = x;
+}
+
+Container.of = function(x) { return new Container(x); };
+Container.prototype.map = function(f){
+    return Container.of(f(this.__value))
+}
